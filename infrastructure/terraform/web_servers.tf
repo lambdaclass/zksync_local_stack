caa7a3a5ea6836c668b8ee3d7dc1096f4de5bb81
Milton <milton.scuderi@lambdaclass.com>
2023-11-02 18:29:21 -0300

    Explorer yml

--- infrastructure/terraform/web_servers.tf
+++ infrastructure/terraform/web_servers.tf
@@ -1,6 +1,6 @@
 resource "hcloud_server" "web" {
   count       = var.instances
-  name        = "zksync-${count.index}"
+  name        = "zksync-server-${count.index}"
   image       = var.os_type
   server_type = var.server_type
   location    = var.location
@@ -8,5 +8,18 @@ resource "hcloud_server" "web" {
   labels = {
     type = "web"
   }
-  user_data = file("user_data.yml")
+  user_data = file("server.yml")
+}
+
+resource "hcloud_server" "web" {
+  count       = var.instances
+  name        = "zksync-explorer-${count.index}"
+  image       = var.os_type
+  server_type = var.server_type
+  location    = var.location
+  ssh_keys    = [hcloud_ssh_key.default.id]
+  labels = {
+    type = "web"
+  }
+  user_data = file("explorer.yml")
 }
